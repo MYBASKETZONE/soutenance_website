18d6fe2fd92e50f8d6b31dbb5dd13456a0429414
e-mandy <andymfrd02@gmail.com>
2026-06-13 13:01:43 +0100

    feat(media): Added media for the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import "./App.css";
-import { useReveal } from './components/ui';
-import { OrderProvider, OrderModal, CartDrawer } from './components/shop';
-import { TopMarquee, Nav, Hero, BrandStrip } from './components/sections-top.v2';
-import { Problem, Solution, Principle, Collection, Interstitial, Binome } from './components/sections-body.v2';
-import { CountdownSection, Testimonials, CtaBand, Footer } from './components/sections-end.v2';
+import { useReveal } from "./components/ui";
+import { OrderProvider, OrderModal, CartDrawer } from "./components/shop";
+import { TopMarquee, Nav, Hero } from "./components/sections-top.v2";
+import {
+  Problem,
+  Solution,
+  Principle,
+  Collection,
+  Interstitial,
+  Binome,
+} from "./components/sections-body.v2";
+import {
+  CountdownSection,
+  Testimonials,
+  CtaBand,
+  Footer,
+} from "./components/sections-end.v2";
 
 function App() {
   useReveal();
@@ -13,7 +25,6 @@ function App() {
       <Nav />
       <main>
         <Hero />
-        <BrandStrip />
         <Problem />
         <Solution />
         <Principle />
